aa215707b4891373f523c8da7f2607a26c3ab53c
soncrayon <arnold.curtis@gmail.com>
2021-12-07 21:06:38 -0500

    add'l quick test f(x) in sum of squares

--- ch_1/sum_of_squares.py
+++ ch_1/sum_of_squares.py
@@ -8,4 +8,11 @@ def sum_of_squares( num ):
         current_num -= 1
     return square_sum
 
-print(sum_of_squares(5))
+# print(sum_of_squares(5))
+
+def quick_test( vals ):
+    for i in range(len(vals)):
+        vals[i] **= 2
+    return sum(vals)
+
+print(quick_test([1, 2, 3]))
